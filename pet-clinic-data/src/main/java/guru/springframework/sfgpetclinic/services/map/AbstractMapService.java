d7d5624c134c5823c81e69e71ab9d456c5a7303f
Jason Taylor <jason.taylor@prudential.com>
2020-09-08 17:06:31 -0400

    closes #8.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>;
+    protected Map<ID, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
